b3487ecdcba68fad9668e6235a49b7f4ba8918b1
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-05-01 21:11:04 -0500

    update LEDdisplay.pi

--- LEDdisplay.py
+++ LEDdisplay.py
@@ -31,39 +31,3 @@ myButton = tkinter.Button(root, text='Enter',command=get_msg)
 myButton.pack(pady=10)
 
 root.mainloop()
-
-'''
-# import libraries for serial port and Tkinter GUI
-import serial
-import Tkinter
- 
-# Open serial port
-ser = serial.Serial('/dev/ttyUSB0', baudrate=9600, timeout=1)
- 
-# Create the root window
-root = Tkinter.Tk()
-root.geometry('400x200+100+100')
-root.title('Serial Keystoke Sender')
- 
-# Create a keystroke handler
-def key(event):
-    if (event.char == 'q'):
-        root.quit()
-    elif event.char >= '0' and event.char <= '9':
-        print 'keystroke:', repr(event.char)
-        ser.write(event.char)
- 
-# Create a label with instructions
-label = Tkinter.Label(root, width=400, height=300, text='Press 0-9 to send a number, or "q" to quit')
-label.pack(fill=Tkinter.BOTH, expand=1)
-label.bind('<Key>', key)
-label.focus_set()
- 
-# Hand over to the Tkinter event loop
-root.mainloop()
- 
-# Close serial port
-ser.close()
-
-
-'''
