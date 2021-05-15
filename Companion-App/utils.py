71e705b9df8fbbb0f67cbe62321a13404dcb0623
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-05-14 23:57:09 -0500

    update main.py

--- Companion-App/utils.py
+++
@@ -1,9 +0,0 @@
-import serial.tools.list_ports  
-
-def find_serial_port():
-    myport = False 
-    ports = list(serial.tools.list_ports.comports())
-    for p in ports:
-        if 'USB-SERIAL CH340' in p[1]:
-            myport = p[0]
-    return myport
