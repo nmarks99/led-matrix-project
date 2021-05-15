7650bac8ce9508b3dea3ebae93ce0de5d61fbb78
nmarks99 <nicholasmarks2022@u.northwestern.edu>
2021-05-14 20:51:11 -0500

    update

--- Companion-App/LEDdisplay.py
+++ Companion-App/LEDdisplay.py
@@ -1,25 +1,38 @@
 from os.path import supports_unicode_filenames
 import serial
 import time  
-import utils
+import serial.tools.list_ports  
 
-p = find_serial_port()
+def find_serial_port():
+    myport = False 
+    ports = list(serial.tools.list_ports.comports())
+    for p in ports:
+        if 'USB-SERIAL CH340' in p[1]:
+            myport = p[0]
 
-# try:
-#     S = serial.Serial('COM8',57600) # Open the serial port
-# except:
-#     print('Device not found')
-#     time.sleep(2)
-#     exit()
+    if not myport:
+        print('No serial devices detected')
 
-# while True:
-#     msg = input('Enter a message: ')
-#     if msg == 'q':
-#         break
-#     else:
-#         msg = msg + '\n'
-#         S.write(msg.encode('utf-8'))
+    return myport
 
-# time.sleep(0.5)
-# S.close()
+myport = find_serial_port()
+print(myport)
+
+try:
+    S = serial.Serial(myport,57600) # Open the serial port
+except:
+    print('Device not found')
+    time.sleep(2)
+    exit()
+
+while True:
+    msg = input('Enter a message: ')
+    if msg == 'q':
+        break
+    else:
+        msg = msg + '\n'
+        S.write(msg.encode('utf-8'))
+
+time.sleep(0.5)
+S.close()
 
